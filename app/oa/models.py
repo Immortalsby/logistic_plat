d4b541074bedfcde4de63cb74cd692e893170e7f
sby945913@gmail.com <sby945913@gmail.com>
2021-07-06 20:10:24 +0800

    po change

--- app/oa/models.py
+++ app/oa/models.py
@@ -40,7 +40,8 @@ def prepare_data(results, g_send_form):
     xml_data['address1'] = results[0]['ShipAdress']
     xml_data['items'] = items
     xml_data['company'] = results[0]['KHMC']
-    xml_data['order_desc'] = "PO Number: " + po_number = 'Not recorded' if results[0]['KHPOH'] == None else results[0]['KHPOH'] + '\n' + g_send_form['remarks']
+    po_number = 'Not recorded' if results[0]['KHPOH'] == None else results[0]['KHPOH']
+    xml_data['order_desc'] = "PO Number: " + po_number + '\n' + g_send_form['remarks']
 
     return json.dumps(xml_data,ensure_ascii=False)
 
